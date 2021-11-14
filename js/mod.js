19b00fceac3ca409e8c88c842d0e85c11deaff7a
Nvidia RTXT2500 <78957603+RTXT25@users.noreply.github.com>
2021-11-14 12:35:35 +1100

    Not at sk but ok

--- js/mod.js
+++ js/mod.js
@@ -13,14 +13,16 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "addition",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
+        <h3>v0.1 Addition</h3><br>
+	        - Added Addition and notifications layer<br>
+		- Also added Adition Upgrades and a buyable<br>
 	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+		- Created the thing`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
